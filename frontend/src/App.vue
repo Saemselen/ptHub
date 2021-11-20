ffc4f06fffc79185f66da6802ba05e2044f4fbe3
samo <in18mosa@tfbern.ch>
2021-11-20 12:50:01 +0100

    project_init

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="root">
+
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue"
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  setup() {
+    const data = reactive({})
+  }
+}
+</script>
+
+<style>
+* {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin:0;
+  padding:0;
+}
+</style>
